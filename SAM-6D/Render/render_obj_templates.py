6aa7837abaebb58611e5c008dd9b338f21b0077c
yizhou <yizhou_zhao@berkeley.edu>
2025-09-25 11:00:09 -0700

    add example

--- SAM-6D/Render/render_obj_templates.py
+++ SAM-6D/Render/render_obj_templates.py
@@ -9,6 +9,7 @@ import trimesh
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--obj_path', type=str, help="The path of obj model")
+parser.add_argument('--cad_path', type=str, help="The path of cad model")
 parser.add_argument('--output_dir', type=str, help="The path to save CAD templates")
 args = parser.parse_args()
 
@@ -18,7 +19,7 @@ print(args.output_dir)
 # set the cnos camera path
 render_dir = os.path.dirname(os.path.abspath(__file__))
 cnos_cam_fpath = os.path.join(render_dir, '../Instance_Segmentation_Model/utils/poses/predefined_poses/cam_poses_level0.npy')
-cnos_cam_fpath = "/home/yizhou/Projects/SAM-6D/SAM-6D/Instance_Segmentation_Model/utils/poses/predefined_poses/cam_poses_level0.npy"
+# cnos_cam_fpath = "/home/yizhou/Projects/SAM-6D/SAM-6D/Instance_Segmentation_Model/utils/poses/predefined_poses/cam_poses_level0.npy"
 
 bproc.init()
 
@@ -85,7 +86,36 @@ for idx, cam_pose in enumerate(cam_poses):
     # save mask
     mask_0 = data["nocs"][0][..., -1]
     cv2.imwrite(os.path.join(save_fpath,'mask_'+str(idx)+'.png'), mask_0*255)
+
+    ################################################## PLY Templates
+
+    bproc.clean_up()
+
+    # load object
+    obj = bproc.loader.load_obj(args.cad_path)[0]
+    obj.set_scale([scale, scale, scale])
+    obj.set_cp("category_id", 1)
+
+    # convert cnos camera poses to blender camera poses
+    cam_pose[:3, 1:3] = -cam_pose[:3, 1:3]
+    cam_pose[:3, -1] = cam_pose[:3, -1] * 0.001 * 2
+    bproc.camera.add_camera_pose(cam_pose)
+
+    # set light
+    light_scale = 2.5
+    light_energy = 1000
+    light1 = bproc.types.Light()
+    light1.set_type("POINT")
+    light1.set_location([light_scale*cam_pose[:3, -1][0], light_scale*cam_pose[:3, -1][1], light_scale*cam_pose[:3, -1][2]])
+    light1.set_energy(light_energy)
+
+    bproc.renderer.set_max_amount_of_samples(50)
+
+    # render the whole pipeline
+    data = bproc.renderer.render()
+    # render nocs
+    data.update(bproc.renderer.render_nocs())
     
-    # save nocs
-    xyz_0 = 2*(data["nocs"][0][..., :3] - 0.5)
-    np.save(os.path.join(save_fpath,'xyz_'+str(idx)+'.npy'), xyz_0.astype(np.float16))
+    # # save nocs
+    # xyz_0 = 2*(data["nocs"][0][..., :3] - 0.5)
+    # np.save(os.path.join(save_fpath,'xyz_'+str(idx)+'.npy'), xyz_0.astype(np.float16))
